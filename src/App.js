efbdfb3fe3cb6fbca61e75cdaac38b618eef1311
fdavison1 <fred.noreply@gmail.com>
2019-12-09 12:37:11 -0700

    example-2

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './examples/Example1.css'
 import Example1 from './examples/Example1'
+import Example2 from './examples/Example2'
 
 function App() {
   return (
     <div className="App">
       
       <Example1 />
+      <Example2 />
    
     </div>
   );
